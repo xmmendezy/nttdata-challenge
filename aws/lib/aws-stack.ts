02847af7d2eea7df842c281f0ac8a1b02e19dcc6
Xavier Méndez Yánez <info@xavicode.com>
2023-05-01 14:51:13 -0500

    Github actions -- init

---
+++ aws/lib/aws-stack.ts
@@ -0,0 +1,18 @@
+import * as cdk from 'aws-cdk-lib';
+import * as eks from 'aws-cdk-lib/aws-eks';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import { Construct } from 'constructs';
+
+export class AwsStack extends cdk.Stack {
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
+
+		new eks.Cluster(this, 'nttdata-cluster', {
+			version: eks.KubernetesVersion.V1_21,
+		});
+
+		new ecr.Repository(this, 'nttdata-repository', {
+			repositoryName: 'nttdata-repository',
+		});
+	}
+}
